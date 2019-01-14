0c94b5d8105c2d951c2c117ce10cbe709327be01
Hazrat Ali <iloveyii@yahoo.com>
2019-01-14 21:34:21 +0100

    Added readme and screeshot png

---
+++ README.md
@@ -0,0 +1,16 @@
+JS - OOP
+===============================
+
+This is a JavaScript tutorial about some very interesting topics.
+
+
+![Screenshot](http://jsoop.softhem.se/screenshot.png)
+
+# [Demo](http://jsoop.softhem.se/)
+
+INSTALLATIONS
+---------------
+  * Clone the repository `git clone git@github.com:iloveyii/js_oop.git`.
+  * Point web browser to index.php directory or Create a virtual host using [vh](https://github.com/iloveyii/vh) `vh new jsoop -p ~/jsoop/index.php`
+  * Browse to [http://jsoops.loc](http://jsoop.loc) 
+  
--- index.php
+++ index.php
@@ -1,79 +1,86 @@
+
+
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>OO Javascript</title>
-    <!-- Css -->
-    <link href="https://bootswatch.com/cosmo/bootstrap.css" rel="stylesheet">
-    <link media="all" type="text/css" rel="stylesheet" href="http://newsletter.softhem.se/css/site.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="https://getbootstrap.com/favicon.ico">
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <!-- Js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Signin Template for Bootstrap</title>
 
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/4.0/examples/sign-in/signin.css" rel="stylesheet">
 </head>
+
 <body>
-<div class="wrapper">
+<div class="row">
+    <div class="col-12">
+        <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+        <h2>The principles of OO JS - By Nicholas</h2>
+        <hr />
+        <ol>
+            <?php
 
-    <section id="main">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <hr />
-                    <h2>The principles of OO JS - By Nicholas</h2>
-                    <hr />
-                    <h1>Index
-                        <ol>
-                        <?php
+            function sortFiles($files)
+            {
+                foreach($files as $file) {
+                    if (pathinfo($file, PATHINFO_EXTENSION) == 'html') {
+                        preg_match('/_(\d+)\.html/', $file, $matches);
+                        if(isset($matches[1])) {
+                            $sorted[$matches[1]] = $file;
+                        } else {
+                            $sorted[] = $file;
+                        }
+                    }
+                }
 
-                            function sortFiles($files)
-                            {
-                                foreach($files as $file) {
-                                    if (pathinfo($file, PATHINFO_EXTENSION) == 'html') {
-                                        preg_match('/_(\d+)\.html/', $file, $matches);
-                                        if(isset($matches[1])) {
-                                            $sorted[$matches[1]] = $file;
-                                        } else {
-                                            $sorted[] = $file;
-                                        }
-                                    }
-                                }
+                ksort($sorted);
+                return $sorted;
+            }
 
-                                ksort($sorted);
-                                return $sorted;
-                            }
+            $dirs = scandir('./');
 
-                            $dirs = scandir('./');
+            foreach($dirs as $dir) {
+                if(in_array($dir, ['chapter_01', 'chapter_02', 'chapter_03', 'chapter_04', 'chapter_05'])) {
+                    echo '<li>' . ucfirst(str_replace('_', ' ', $dir)) . '</li>';
+                    $files = scandir("./{$dir}");
+                    $files = sortFiles($files);
 
-                            foreach($dirs as $dir) {
-                                if(in_array($dir, ['chapter_01', 'chapter_02', 'chapter_03', 'chapter_04', 'chapter_05'])) {
-                                    echo '<li>' . ucfirst(str_replace('_', ' ', $dir)) . '</li>';
-                                    $files = scandir("./{$dir}");
-                                    $files = sortFiles($files);
-
-                                    echo '<ol>';
-                                    foreach ($files as $file) {
-                                        echo "<li class='list'><a href='{$dir}/{$file}' >" . preg_replace('/[^a-z]/i', ' ', ucfirst(rtrim($file, '.html'))) . '</a></li>';
-                                    }
-                                    echo '</ol>';
-                                }
-                            }
-                        ?>
-                        </ol>
-                </div>
-            </div>
-        </div>
-    </section>
-
-</div>
-
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; SoftHem 2017</p>
-        <p class="pull-right">Powered by <a href="http://www.softhem.se/" rel="external">SoftHem</a></p>
+                    echo '<ol>';
+                    foreach ($files as $file) {
+                        echo "<li class='list'><a href='{$dir}/{$file}' >" . preg_replace('/[^a-z]/i', ' ', ucfirst(rtrim($file, '.html'))) . '</a></li>';
+                    }
+                    echo '</ol>';
+                }
+            }
+            ?>
+        </ol>
+        <p class="mt-5 mb-3 text-muted">&copy; 2019-2020</p>
     </div>
-</footer>
+</div>
 
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
